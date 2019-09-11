580963c5c25e6366e056837723f8e2e56467e4e4
Gergely VAKULYA <vakulya@dcs.uni-pannon.hu>
2019-09-12 00:00:12 +0200

    Loop and Z added

--- tester.sh
+++ tester.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-curl -X POST -d '{"x":200,"y":100}' http://localhost:8088
+while true
+  do
+    x=$(echo "($RANDOM / 1000)-15" | bc -l)
+    y=$(echo "($RANDOM / 1000)-15" | bc -l)
+    z=$(echo "($RANDOM / 1000)-15" | bc -l)
+    curl -X POST -d '{"x":"'$x'","y":"'$y'","z":"'$z'"}' http://localhost:8088
+  done
